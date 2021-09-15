005f323186f3d5d4a4169b2d7c2822b8f577f050
Karan2021 <90594223+Karan2021@users.noreply.github.com>
2021-09-15 15:22:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class consoleapp3
+{
+    public static void Main()
+    {
+        string fileName = @"mytest.txt";
+        try
+        {
+            Console.Write("\n\n A file created with name mytest.txt\n");
+            using (FileStream fileStr = File.Create(fileName))
+            {
+                Console.WriteLine("A file created with name mytest.txt");
+            }
+        }
+        catch (Exception MyExcep)
+        {
+            Console.WriteLine(MyExcep.ToString());
+        }
+    }
+}
